b17f801171d72e0887a1897ddcb88a1bf7c63d12
Andi Heinkelein <andifeind@noname-media.com>
2018-10-26 17:51:36 +0200

    WIP argv parser

--- example/supercmd.js
+++ example/supercmd.js
@@ -3,4 +3,5 @@ const cmd = require('../')
 cmd
   .usage('cmdtest [options]')
   .description('Shows a SuperCommand description')
+  .option('-n, --name <str>', 'Add a name')
   .parse(process.argv)

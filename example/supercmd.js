61c935c91324e422b6a714f9f88063de1700c4a6
Andi Heinkelein <andifeind@noname-media.com>
2018-10-30 09:25:25 +0100

    WIP action handling

--- example/supercmd.js
+++ example/supercmd.js
@@ -1,7 +1,31 @@
-const cmd = require('../')
+const supercmd = require('../')
 
-cmd
-  .usage('cmdtest [options]')
+supercmd
+  .cmd('cmdtest [options]')
   .description('Shows a SuperCommand description')
   .option('-n, --name <str>', 'Add a name')
+  .action(() => {
+    console.log('Action: cmdtest')
+    console.log('CTX', ctx)
+  })
+  .parse(process.argv)
+
+supercmd
+  .cmd('foo <str>')
+  .description('Shows a SuperCommand description')
+  .option('-n, --name <str>', 'Add a name')
+  .action((ctx, bar) => {
+    console.log('Action: foo', bar)
+    console.log('CTX', ctx)
+  })
+  .parse(process.argv)
+
+supercmd
+  .cmd('*')
+  .description('Shows a SuperCommand description')
+  .option('-n, --name <str>', 'Add a name')
+  .action((ctx, bar) => {
+    console.log('Action: default', bar)
+    console.log('CTX', ctx)
+  })
   .parse(process.argv)
